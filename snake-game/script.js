805fd0d8a255bf4d8fa0ffb621317dae1f99dea2
ditaoktaviari <ditaoktaviari@gmail.com>
2022-03-13 16:39:24 +0800

    Add level and snake speed change

--- snake-game/script.js
+++ snake-game/script.js
@@ -12,7 +12,7 @@ const DIRECTION = {
     UP: 2,
     DOWN: 3,
 }
-const MOVE_INTERVAL = 200;
+let MOVE_INTERVAL = 120;
 
 function initPosition() {
     return {
@@ -41,10 +41,52 @@ let snake1 = {
     position: initPosition(),
     direction: initDirection(),
     score: 0,
+    level: 1,
 }
 let apple1 = {
     color: "red",
     position: initPosition(),
+}
+
+function initWall(x, y, width, height, color){
+    return{
+        x: x,
+        y: y,
+        width: width,
+        height: height,
+        color:color,
+    }
+}
+let wall1 = initWall(50, 130, 300, 20, "purple");
+let wall2 = initWall(50, 200, 300, 20, "purple");
+let wall3 = initWall(50, 270, 300, 20, "purple");
+let wall4 = initWall(100, 50, 20, 300, "purple");
+let wall5 = initWall(280, 50, 20, 300, "purple");
+
+function drawWall(ctx, x, y, width, height, color) {
+    ctx.strokeStyle = color;
+    ctx.rect(x, y, width, height);
+    ctx.stroke();
+}
+
+function drawLevel(snake) {
+    let levelCanvas = document.getElementById("levelBoard");
+    let levelCtx = levelCanvas.getContext("2d");
+
+    levelCtx.clearRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+    levelCtx.font = "15px Arial";
+    levelCtx.fillStyle = "green"
+    levelCtx.fillText("Level : " + snake.level, 10, levelCanvas.scrollHeight / 2);
+}
+
+function drawSpeed() {
+    let speedCanvas = document.getElementById("speedBoard");
+    let speedCtx = speedCanvas.getContext("2d");
+
+    speedCtx.clearRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+    speedCtx.font = "15px Arial";
+    speedCtx.fillStyle = "green"
+    speedCtx.fillText("Speed : " + MOVE_INTERVAL + " ms", 10, speedCanvas.scrollHeight / 2);
 }
 
 function drawCell(ctx, x, y, color) {
@@ -119,7 +161,53 @@ function draw() {
         drawCell(ctx, apple1.position.x, apple1.position.y, apple1.color);
       
         drawScore(snake1);
+
+        drawLevel(snake1);
+        if(snake1.level == 2){
+            drawWall(ctx, wall1.x, wall1.y, wall1.width, wall1.height, wall1.color);
+        }else if(snake1.level == 3){
+            drawWall(ctx, wall1.x, wall1.y, wall1.width, wall1.height, wall1.color);
+            drawWall(ctx, wall2.x, wall2.y, wall2.width, wall2.height, wall2.color);
+        }else if(snake1.level == 4){
+            drawWall(ctx, wall1.x, wall1.y, wall1.width, wall1.height, wall1.color);
+            drawWall(ctx, wall2.x, wall2.y, wall2.width, wall2.height, wall2.color);
+            drawWall(ctx, wall3.x, wall3.y, wall3.width, wall3.height, wall3.color);
+        }else if(snake1.level == 5){
+            drawWall(ctx, wall4.x, wall4.y, wall4.width, wall4.height, wall4.color);
+            drawWall(ctx, wall5.x, wall5.y, wall5.width, wall5.height, wall5.color);
+        }
+
+        drawSpeed();
     }, REDRAW_INTERVAL);
+}
+
+function level(snake){
+    if(snake.score == 5 && snake.level == 1){
+        alert("Level 1 Complate!");
+        snake.level = 2;
+        MOVE_INTERVAL = 100;
+    }
+    else if(snake.score == 10 && snake.level == 2){
+        alert("Level 2 Complate!");
+        snake.level = 3;
+        MOVE_INTERVAL = 80;
+    }
+    else if(snake.score == 15 && snake.level == 3){
+        alert("Level 3 Complate!");
+        snake.level = 4;
+        MOVE_INTERVAL = 60;
+    }
+    else if(snake.score == 20 && snake.level == 4){
+        alert("Level 4 Complate!");
+        snake.level = 5;
+        MOVE_INTERVAL = 40;
+    }
+    else if(snake.score == 25 && snake.level == 5){
+        alert("Game Over, Try Again?");
+        snake.level = 1;
+        snake.score = 0;
+        MOVE_INTERVAL = 120;
+    } 
 }
 
 function teleport(snake) {
@@ -150,24 +238,28 @@ function moveLeft(snake) {
     snake.head.x--;
     teleport(snake);
     eat(snake, apple1);
+    level(snake);
 }
 
 function moveRight(snake) {
     snake.head.x++;
     teleport(snake);
     eat(snake, apple1);
+    level(snake);
 }
 
 function moveDown(snake) {
     snake.head.y++;
     teleport(snake);
     eat(snake, apple1);
+    level(snake);
 }
 
 function moveUp(snake) {
     snake.head.y--;
     teleport(snake);
     eat(snake, apple1);
+    level(snake);
 }
 
 function move(snake) {
